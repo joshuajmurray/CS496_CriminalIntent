0b14841ab349949067ea02d2747269a227611c9c
joshua murray <joshua@arcflashllc.com>
2017-10-06 20:13:22 -0700

    added Crime class

---
+++ app/src/main/java/com/jmurray/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.jmurray.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Joshua on 10/6/2017.
+ */
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
