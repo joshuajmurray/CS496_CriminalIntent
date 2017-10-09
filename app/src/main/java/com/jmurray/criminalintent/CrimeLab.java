119c20a2073e0357b38cd25f2f4310113617dd5d
joshua murray <joshua@arcflashllc.com>
2017-10-09 09:00:13 -0700

    implemented CrimeLab class fully

--- app/src/main/java/com/jmurray/criminalintent/CrimeLab.java
+++ app/src/main/java/com/jmurray/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.jmurray.criminalintent;
 
 import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Joshua on 10/9/2017.
@@ -8,6 +11,8 @@ import android.content.Context;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if(sCrimeLab == null) {
@@ -17,6 +22,25 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for(int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);//every other
+            mCrimes.add(crime);
+        }
+    }
 
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for(Crime crime : mCrimes) {
+            if(crime.getID().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 }
