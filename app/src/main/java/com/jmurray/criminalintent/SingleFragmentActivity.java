0fa0cad4ca5880944f306ba189e19835ed4c954b
joshua murray <joshua@arcflashllc.com>
2017-10-15 22:18:57 -0700

    added abstract class

--- app/src/main/java/com/jmurray/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/jmurray/criminalintent/SingleFragmentActivity.java
@@ -1,11 +1,16 @@
 package com.jmurray.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+/**
+ * Created by Joshua on 10/15/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity{
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,7 @@ public class CrimeActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if(fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
     }
