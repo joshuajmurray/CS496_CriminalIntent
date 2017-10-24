943f522bd25ec7a3aa05b23f4f7199c98b9828a9
joshua murray <joshua@arcflashllc.com>
2017-10-23 22:52:38 -0700

    added pager, added crimePagerActivity to manifest

--- app/src/main/java/com/jmurray/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/jmurray/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.jmurray.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +9,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Joshua on 10/23/2017.
@@ -14,13 +17,23 @@ import java.util.List;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.jmurray.criminalintent.crime_id";
+
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
 
