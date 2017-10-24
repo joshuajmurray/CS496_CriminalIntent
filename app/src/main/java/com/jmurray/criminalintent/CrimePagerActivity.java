80bc4f830aae217bfc2516bb949107218d039813
joshua murray <joshua@arcflashllc.com>
2017-10-23 23:09:54 -0700

    finished chap 11

--- app/src/main/java/com/jmurray/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/jmurray/criminalintent/CrimePagerActivity.java
@@ -51,5 +51,12 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        for(int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getID().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
