27f0dcd048ec5053529d513bc30813c635df54e6
joshua murray <joshua@arcflashllc.com>
2017-10-23 18:15:32 -0700

    chap 11 first commit

---
+++ app/src/main/java/com/jmurray/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,42 @@
+package com.jmurray.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import java.util.List;
+
+/**
+ * Created by Joshua on 10/23/2017.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getID());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+}
