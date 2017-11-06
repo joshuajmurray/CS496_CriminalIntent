08de70a439e1e382d296135f877a2f29e61d1185
joshua murray <joshua@arcflashllc.com>
2017-11-05 22:49:44 -0800

    first commit chap 15, added column to db

--- app/src/main/java/com/jmurray/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/jmurray/criminalintent/database/CrimeCursorWrapper.java
@@ -3,6 +3,8 @@ package com.jmurray.criminalintent.database;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 import com.jmurray.criminalintent.Crime;
+import com.jmurray.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -16,15 +18,17 @@ public class CrimeCursorWrapper extends CursorWrapper {
     }
 
     public Crime getCrime() {
-        String uuidString = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.UUID));
-        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.TITLE));
-        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.DATE));
-        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SOLVED));
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+        String suspect = getString(getColumnIndex(CrimeTable.Cols.SUSPECT));
 
         Crime crime = new Crime(UUID.fromString(uuidString));
         crime.setTitle(title);
         crime.setDate(new Date(date));
         crime.setSolved(isSolved != 0);
+        crime.setSuspect(suspect);
 
         return crime;
     }
